5a492366cf654214cfcaad8ea5a61f9fe32659ab
Nedoloogiy <Nedoloogiy@mail.ru>
2019-12-18 16:36:40 +0300

    add pig

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 document.getElementsByTagName("body")[0].insertAdjacentHTML('beforeEnd', '<img src="img/orange.png" id="orange">');
 var orange = document.getElementById("orange");
 orange.style.position = 'fixed';
+
+// работаем со свиньей
+document.getElementsByTagName("body")[0].insertAdjacentHTML('beforeEnd', '<img src="img/pig.png" id="pig">');
