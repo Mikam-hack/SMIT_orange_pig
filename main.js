3507501fffc1c4dc98b8f60c3351585370bc2627
Nedoloogiy <Nedoloogiy@mail.ru>
2019-12-18 16:44:39 +0300

    add mouseMove event listener

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ orange.style.position = 'fixed';
 document.getElementsByTagName("body")[0].insertAdjacentHTML('beforeEnd', '<img src="img/pig.png" id="pig">');
 var pig = getElementById('pig');
 pig.style.position = 'fixed';
+
+var mouseListener = function(event){mouseMoveFunc(event)}
+document.addEventListener('mousemove', mouseListener);
+
+function mouseMoveFunc(event){
+	pig.style.left = event.clientX - 64 + 'px';
+	pig.style.top = event.clientY - 64 +'px';
+}
+
