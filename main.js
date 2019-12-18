7d08b9f69863567ef077b47830b524c498b26bb8
Nedoloogiy <Nedoloogiy@mail.ru>
2019-12-18 16:34:11 +0300

    orange style position

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 document.getElementsByTagName("body")[0].insertAdjacentHTML('beforeEnd', '<img src="img/orange.png" id="orange">');
 var orange = document.getElementById("orange");
+orange.style.position = 'fixed';
